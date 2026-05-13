edca21828245e33b2358835efe17bf1f1aa63b39
chai_un <un7476@naver.com>
2026-05-13 13:54:20 +0900

    modify test, train code

--- engine/rl/test.py
+++ engine/rl/test.py
@@ -1,7 +1,7 @@
 from sb3_contrib import MaskablePPO
 import matplotlib.pyplot as plt
 import numpy as np
-from datacenter import DataCenterEnv
+from engine.rl.datacenter import DataCenterEnv
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 
 
@@ -36,9 +36,8 @@ def visualize(env, title="Data Center"):
     plt.tight_layout()
     plt.show()
 
+
 def visualize_advanced(env, step, reward=None):
-    import matplotlib.pyplot as plt
-    import numpy as np
 
     heat = env.temp
     rack = env.rack_map
@@ -71,11 +70,11 @@ def visualize_advanced(env, step, reward=None):
         YY,
         U,
         V,
-        mag,                 # 색 = 세기
+        mag,  # 색 = 세기
         cmap="cool",
         scale=25,
         width=0.003,
-        alpha=0.9
+        alpha=0.9,
     )
 
     # =========================
@@ -85,16 +84,10 @@ def visualize_advanced(env, step, reward=None):
     plt.scatter(xs, ys, c="blue", s=50, label="Rack")
 
     # ➡️ rack 방향
-    dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+    dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     for y, x in zip(ys, xs):
         d = dirs[int(env.rack_dir[y, x])]
-        plt.arrow(
-            x, y,
-            d[1]*0.8,
-            d[0]*0.8,
-            color="cyan",
-            head_width=0.5
-        )
+        plt.arrow(x, y, d[1] * 0.8, d[0] * 0.8, color="cyan", head_width=0.5)
 
     # =========================
     # ❄️ cooling
@@ -124,6 +117,7 @@ def visualize_advanced(env, step, reward=None):
     plt.tight_layout()
     plt.show()
 
+
 def make_env():
     def _init():
         env = DataCenterEnv(grid_size=50, rack_num=10)
@@ -132,23 +126,35 @@ def make_env():
     return _init
 
 
-PATH = "./ppo_logs/ppo_dc_20260430_164035/"
+PATH = "./ppo_logs/ppo_dc_20260506_175459/"
+grid = np.zeros((50, 50))
+grid[:40, :] = 1
+grid[:, 10:] = 1
+cooler = np.array([[45, 5], [45, 6], [46, 5], [46, 6]])
+options = {
+    "obstacle": grid,
+    "rack_num": 50,
+    "cooling_pos": cooler,
+}
 
-env = DummyVecEnv([make_env()])
-env = VecNormalize.load(f"{PATH}final_vecnormalize.pkl", env)
-env.training = False
-env.norm_reward = False
 
-model = MaskablePPO.load(f"{PATH}final_model", env=env)
-obs = env.reset()
-done = False
-step = 0
-while not done and step < 100:
-    action_masks = env.envs[0].action_masks()
-    print("Action mask sum:", action_masks.sum())
-    action, _ = model.predict(obs, deterministic=True, action_masks=action_masks)
-    obs, reward, done, _ = env.step(action)
-    done = done[0]
-    step += 1
-    if step == 9:
-        visualize_advanced(env.envs[0], step)
+def test():
+    env = DummyVecEnv([make_env()])
+    env = VecNormalize.load(f"{PATH}center_15000000_steps_vecnormalize.pkl", env)
+    env.training = False
+    env.norm_reward = False
+
+    model = MaskablePPO.load(f"{PATH}center_15000000_steps", env=env)
+    obs, _ = env.envs[0].reset(options=options)
+    obs = env.normalize_obs(obs)
+    done = False
+    step = 0
+    while not done:
+        action_masks = env.envs[0].action_masks()
+        print("Action mask sum:", action_masks.sum())
+        action, _ = model.predict(obs, deterministic=True, action_masks=action_masks)
+        obs, reward, done, _, _ = env.envs[0].step(action)
+        # done = done[0]
+        step += 1
+        if step == env.envs[0].rack_num - 1:
+            visualize_advanced(env.envs[0], step)
--- engine/rl/train.py
+++ engine/rl/train.py
@@ -2,7 +2,7 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3.common.vec_env import VecNormalize, VecMonitor
 from stable_baselines3.common.monitor import Monitor
-from datacenter import DataCenterEnv
+from engine.rl.datacenter import DataCenterEnv
 import wandb
 from wandb.integration.sb3 import WandbCallback
 from stable_baselines3.common.callbacks import CheckpointCallback
@@ -44,7 +44,8 @@ def make_env(rank):
     return _init
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+def train():
     config = {
         "learning_rate": 5e-5,
         "n_steps": 1024,
